5b13ba80bc5a4448e4cebbed7ec64e9ddf611157
Olivier LASBLEIS <lasbleis.olivier@yahoo.fr>
2019-05-21 16:43:31 +0200

    initialisation

---
+++ src/main/java/fr/diginamic/traitement_fichier/App.java
@@ -0,0 +1,13 @@
+package fr.diginamic.traitement_fichier;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
